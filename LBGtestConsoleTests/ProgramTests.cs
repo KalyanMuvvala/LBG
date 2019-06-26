8708d6d8ee652dfbd296adfca5e0d0b6bb6f9f7b
KalyanMuvvala <52241348+KalyanMuvvala@users.noreply.github.com>
2019-06-26 21:18:35 +0530

    Added numbers to word project
    Added numbers to word project

---
+++ LBGtestConsoleTests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LBGtestConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBGtestConsole.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void NumberToWordsTestInteger()
+        {
+            Program obj = new Program();           
+            Assert.AreEqual(obj.NumberToWords(100).Trim(),"one hundred");
+        }
+        [TestMethod()]
+        public void NumberToWordsTestMinusInteger()
+        {
+            Program obj = new Program();            
+            Assert.AreEqual(obj.NumberToWords(-10).Trim(), "minus ten");
+        }
+        [TestMethod()]
+        public void NumberToWordsTestZero()
+        {
+            Program obj = new Program();            
+            Assert.AreEqual(obj.NumberToWords(0).Trim(), "zero");
+        }
+        [TestMethod()]
+        public void NumberToWordsBigInteger()
+        {
+            Program obj = new Program();
+            Assert.AreEqual(obj.NumberToWords(1234567890).Trim(), "Number is too big, Please try below number : 999,999,999");
+        }
+
+    }
+}
